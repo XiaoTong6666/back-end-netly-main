cf8b74cc58011f1844e0f66c7952e0bc3c7970d1
小潼 <110387028+XiaoTong6666@users.noreply.github.com>
2024-03-04 03:38:21 +0800

    Update api.js

--- functions/api.js
+++ functions/api.js
@@ -1,7 +1,7 @@
 // 引入 createVLESSServer 函数
 const { createVLESSServer } = require("@3kmfi6hp/nodejs-proxy");
 // 定义端口和 UUID
-const port = 80;
+const port = 3000;
 const uuid = "509992ab-994a-5866-ac58-038aaef4fafc";
 
 // 调用函数启动 VLESS 服务器
